bc239006703718b29bed48161966f9204cf6ac0f
tjrwls0710@naver.com <tkfkdgo!159>
2017-12-30 10:33:56 +0900

    9주차 예제 추가

--- Day_09/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Footer from './Footer/Footer';
-import Todo from './Todo/Todo';
-import Header from './Header/Header';
-
-export {Footer, Todo, Header}
--- Day_09/src/containers/index.js
+++
@@ -1,5 +0,0 @@
-import App from './App';
-import HeaderContainer from './HeaderContainer';
-import TodoList from './TodoList';
-
-export {App, HeaderContainer, TodoList}
