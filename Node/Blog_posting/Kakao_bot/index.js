ac80fb8dc57a9d0723b1ee3672def2fd87183a7f
tjrwls0710@naver.com <tkfkdgo!159>
2018-01-18 15:41:46 +0900

    블로그 포스팅 카카오 봇 추가

--- Node/Blog_posting/Kakao_bot/index.js
+++ Node/Blog_posting/Kakao_bot/index.js
@@ -1,32 +1,46 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import message from './message';
-
-let app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 app.set('PORT', 8080);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/keyboard', (req, res) => {
+app.get('/keyboard', function(req, res){
   res.set({
     'content-type': 'application/json'
-  }).send(JSON.stringify(message.buttonsType()));
+  }).send(JSON.stringify({
+      type: 'buttons',
+      buttons: ['메뉴1', '메뉴2', '메뉴3']
+    }));
 });
 
-app.post('/message', checkUserKey, (req, res) => {
+app.post('/message', function(req, res){
   const _obj = {
-    user_key: req.body.user_key, // 유저키
+    user_key: req.body.user_key,
     type: req.body.type,
-    content: req.body.content // 눌린 버튼 또는 텍스트
+    content: req.body.content
   };
+
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify(
+    {
+      message: {
+        text: _obj.content,
+      },
+      keyboard: {
+        type: 'buttons',
+        buttons: ['메뉴1', '메뉴2', '메뉴3']
+      }
+    }
+  ));
 });
 
-
-app.listen(app.get('PORT'), () => {
-  console.log(`server start... port : ${app.get('PORT') }`);
+app.listen(app.get('PORT'), function(){
+  console.log("server start... port :" + app.get('PORT'));
 });
-
 
